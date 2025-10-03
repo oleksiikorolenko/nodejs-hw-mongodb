ebe529e51b55ba7c1c67b15e797e9e357f61fb03
Korolenko <korolenkoalexey84@gmail.com>
2025-10-03 13:10:22 +0300

    login commit

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { validateBody } from "../middlewares/validateBody.js";
-import { registerUserSchema } from "../validation/auth.js";
+import { loginUserSchema, registerUserSchema } from "../validation/auth.js";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
-import { registerUserController } from "../controllers/auth.js";
+import { loginUserController, registerUserController } from "../controllers/auth.js";
 
 const router = Router();
 
 router.post('/register', validateBody(registerUserSchema), ctrlWrapper(registerUserController));
+router.post('/login', validateBody(loginUserSchema), ctrlWrapper(loginUserController));
 
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import router from './routers/index.js';
+import cookieParser from 'cookie-parser';
 
 
 const PORT = process.env.PORT || 8080;
@@ -14,7 +15,8 @@ export function setupServer() {
     const app = express();
 
     app.use(express.json());
-    app.use(cors());
+  app.use(cors());
+  app.use(cookieParser());
 
       app.use(
   pino({
